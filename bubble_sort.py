d9230171003bb23769b177a446a2e8bacb2e1910
Shuvam Dutta <80211038+shuvamdnk@users.noreply.github.com>
2021-10-05 00:59:02 +0530

    Delete bubble_sort.py

--- bubble_sort.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(arr):
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                arr[i], arr[j+1] = arr[j+1], arr[i]
-    print(arr) 
-
-bubble_sort([23,12,30,10,4,57])               
